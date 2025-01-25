d1dd592a026d80eb40a816b9ba6375e9e8bca700
BenRapp22 <ben.rapp@temple.edu>
2025-01-25 14:43:51 -0500

    Adding collisions - tests failing;
    Fixed constant comma issue again

---
+++ src/python-server/collisions.py
@@ -0,0 +1,16 @@
+import constants
+
+def applyCollisions(players, bullets):
+    destroyedBullets = []
+
+    for i in range(len(bullets)):
+        for j in range(len(players)):
+            bullet = bullets[i]
+            player = players[i]
+
+            if (bullet.parentID == player.id) and (player.distanceTo(bullet) <= constants.PLAYER_RADIUS):
+                destroyedBullets.append(bullet)
+                player.takeBulletDamage()
+                break
+
+    return destroyedBullets    
---
+++ src/python-server/testCollisions.py
@@ -0,0 +1,36 @@
+from bullet import Bullet
+from collisions import applyCollisions
+from player import Player
+
+import constants
+
+def testApplyCollisions():
+    distance_from_player = constants.BULLET_RADIUS + constants.PLAYER_RADIUS
+    players = [
+        Player('1', 'guest1', 1000, 40),
+        Player('2', 'guest2', 2000, 2000)
+    ]
+    bullets = [
+        Bullet('2', 1000-distance_from_player, 40, 0),
+        Bullet('2', 1000+distance_from_player, 40, 0)
+    ]
+    result = applyCollisions(players, bullets)
+    assert len(result) == 0
+
+def testNoSelfHarm():
+    playerID = '1234'
+    p = Player(playerID, 'guest', 40, 40)
+    b = Bullet(playerID, 40, 40, 0)
+    result = applyCollisions([p], [b])
+    assert len(result) == 0
+
+def testHarmOthers(mocker):
+    p = Player('1', 'guest', 40, 40)
+    b = Bullet('2', 40, 40 + constants.BULLET_RADIUS + constants.PLAYER_RADIUS, 0)
+
+    spy = mocker.spy(p, 'takeBulletDamage')
+
+    result = applyCollisions([p], [b])
+    assert len(result) == 1
+    assert b in result
+    assert spy.call_count == 1
